724ae46d11ee689669d6865b1b3b27fe9508a5b5
msconfiggg <1466876532@qq.com>
2025-02-09 15:33:09 +0800

    lab02 commit

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,11 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    unsigned tmp = *reg & 1;
+    tmp ^= *reg >> 2 & 1;
+    tmp ^= *reg >> 3 & 1;
+    tmp ^= *reg >> 5 & 1;
+    *reg >>= 1;
+    *reg |= tmp << 15;
 }
 
