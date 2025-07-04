53e95cd3e33a943ded227f21acb890e130219617
msconfiggg <1466876532@qq.com>
2025-07-04 20:20:28 +0800

    lab09 commit

--- lab09/simd.c
+++ lab09/simd.c
@@ -51,11 +51,28 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
 	long long int result = 0;				   // This is where you should put your final result!
 	/* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
 	
+	int sums[4];
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
 		/* YOUR CODE GOES HERE */
+		__m128i sum = _mm_setzero_si128();
+		for (unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+			__m128i tmp = _mm_loadu_si128((__m128i *) &vals[i]); // Load 4 integers from vals
+			__m128i mask = _mm_cmpgt_epi32(tmp, _127); // Compare with 127
+			tmp = _mm_and_si128(tmp, mask);
+			sum = _mm_add_epi32(sum, tmp);
+		}
+
+		_mm_storeu_si128((__m128i *) sums, sum);
+		for (unsigned int i = 0; i < 4; i++) {
+			result += sums[i];
+		}
 
 		/* You'll need a tail case. */
-
+		for (unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) {
+				result += vals[i];
+			}
+		}
 	}
 	clock_t end = clock();
 	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
@@ -66,11 +83,52 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
 	clock_t start = clock();
 	__m128i _127 = _mm_set1_epi32(127);
 	long long int result = 0;
+	int sums[4];
 	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
 		/* COPY AND PASTE YOUR sum_simd() HERE */
 		/* MODIFY IT BY UNROLLING IT */
+		/* YOUR CODE GOES HERE */
+		__m128i sum = _mm_setzero_si128();
+		for (unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+			__m128i tmp = _mm_loadu_si128((__m128i *) &vals[i]); // Load 4 integers from vals
+			__m128i mask = _mm_cmpgt_epi32(tmp, _127); // Compare with 127
+			tmp = _mm_and_si128(tmp, mask);
+			sum = _mm_add_epi32(sum, tmp);
 
+			tmp = _mm_loadu_si128((__m128i *) &vals[i + 4]);
+			mask = _mm_cmpgt_epi32(tmp, _127);
+			tmp = _mm_and_si128(tmp, mask);
+			sum = _mm_add_epi32(sum, tmp);
+
+			tmp = _mm_loadu_si128((__m128i *) &vals[i + 8]);
+			mask = _mm_cmpgt_epi32(tmp, _127);
+			tmp = _mm_and_si128(tmp, mask);
+			sum = _mm_add_epi32(sum, tmp);
+
+			tmp = _mm_loadu_si128((__m128i *) &vals[i + 12]);
+			mask = _mm_cmpgt_epi32(tmp, _127);
+			tmp = _mm_and_si128(tmp, mask);
+			sum = _mm_add_epi32(sum, tmp);
+		}
+		
 		/* You'll need 1 or maybe 2 tail cases here. */
+		for (unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS / 4 * 4; i += 4) {
+			__m128i tmp = _mm_loadu_si128((__m128i *) &vals[i]);
+			__m128i mask = _mm_cmpgt_epi32(tmp, _127);
+			tmp = _mm_and_si128(tmp, mask);
+			sum = _mm_add_epi32(sum, tmp);
+		}
+
+		_mm_storeu_si128((__m128i *) sums, sum);
+		for (unsigned int i = 0; i < 4; i++) {
+			result += sums[i];
+		}
+
+		for (unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) {
+				result += vals[i];
+			}
+		}
 
 	}
 	clock_t end = clock();
